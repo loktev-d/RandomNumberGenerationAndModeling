8f8fb62fa6eb381acda0c0ebf3a0fc7323c2e2cf
Washington George <dourch89@gmail.com>
2020-10-07 11:03:12 +0300

    Added interfaces

---
+++ Model/IRandomGenerator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RandomNumberGenerationAndModeling.Model
+{
+    public interface IRandomGenerator
+    {
+        public void Generate();
+    }
+}
---
+++ Model/ITask.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RandomNumberGenerationAndModeling.Model
+{
+    public interface ITask
+    {
+        public void Invoke();
+    }
+}
