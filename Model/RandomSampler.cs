969fcbc760b0a16c29bbaca8f8fe201931b17336
Washington George <dourch89@gmail.com>
2020-11-03 19:41:36 +0300

    Edit view

--- Model/RandomSampler.cs
+++ Model/RandomSampler.cs
@@ -5,20 +5,16 @@ namespace RandomNumberGenerationAndModeling.Model
 {
     public abstract class RandomSampler : RandomGenerator
     {
-        protected List<float> randomGeneratedNumbers;
         protected Random Generator { get; set; }
 
         public RandomSampler(int length) : base(length)
         {
             Length = length;
-            randomGeneratedNumbers = new List<float>();
         }
 
         protected float GenerateRandomNumber()
         {
-            float number = (float) Generator.NextDouble();
-            randomGeneratedNumbers.Add(number);
-            return number;
+            return (float) Generator.NextDouble();
         }
     }
 }
